5366d9baa8f51c2058933782f12051b6075f326b
DenysChebotarov <114431465+DenysChebotarov@users.noreply.github.com>
2023-03-06 16:45:28 +0200

    goit-markup-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player  from '@vimeo/player'; 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+currentTime()
+player.on('timeupdate', throttle(onPlay, 1000)); 
+
+function onPlay(time) {
+    localStorage.setItem(STORAGE_KEY, time.seconds); 
+}
+
+function currentTime() {
+    const currentSecond = localStorage.getItem(STORAGE_KEY);
+    if(currentSecond){
+        player.setCurrentTime(currentSecond) 
+    }
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name = email]');
+const textarea = document.querySelector('textarea[name = message]');
+
+const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const data = (saveData !== null && saveData !== undefined) ? saveData : { email: '', message: '' };
+  
+email.value = data.email;
+textarea.value = data.message;
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (!email.value || !textarea.value) {
+    alert('Заповніть всі поля');
+    return;
+  }
+  console.log(data);
+  email.value = '';
+  textarea.value = '';
+  data.email = '';
+  data.message = '';
+  localStorage.clear();
+}
+
+
+function onFormInput(e) {
+  
+data[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
